093309e0f155db783f3aa5d712db982c7e00f63f
shuke8 <stemirov777@gmail.com>
2023-11-02 11:03:41 +0500

    first commit

---
+++ src/lib/navigation.js
@@ -0,0 +1,56 @@
+export const navigation = [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'UCGP Line A B C',
+    links: [
+      { title: ' Line A 0-57 km', href: '/docs/understanding-caching' },
+      {
+        title: 'Line A 57-349 km',
+        href: '/docs/predicting-user-behavior',
+      },
+      { title: 'Line A 349-527 km', href: '/docs/basics-of-time-travel' },
+      {
+        title: 'Introduction to string theory',
+        href: '/docs/introduction-to-string-theory',
+      },
+      { title: 'The butterfly effect', href: '/docs/the-butterfly-effect' },
+    ],
+  },
+  {
+    title: 'Advanced guides',
+    links: [
+      { title: 'Writing plugins', href: '/docs/writing-plugins' },
+      { title: 'Neuralink integration', href: '/docs/neuralink-integration' },
+      { title: 'Temporal paradoxes', href: '/docs/temporal-paradoxes' },
+      { title: 'Testing', href: '/docs/testing' },
+      { title: 'Compile-time caching', href: '/docs/compile-time-caching' },
+      {
+        title: 'Predictive data generation',
+        href: '/docs/predictive-data-generation',
+      },
+    ],
+  },
+  {
+    title: 'API reference',
+    links: [
+      { title: 'CacheAdvance.predict()', href: '/docs/cacheadvance-predict' },
+      { title: 'CacheAdvance.flush()', href: '/docs/cacheadvance-flush' },
+      { title: 'CacheAdvance.revert()', href: '/docs/cacheadvance-revert' },
+      { title: 'CacheAdvance.regret()', href: '/docs/cacheadvance-regret' },
+    ],
+  },
+  {
+    title: 'Contributing',
+    links: [
+      { title: 'How to contribute', href: '/docs/how-to-contribute' },
+      { title: 'Architecture guide', href: '/docs/architecture-guide' },
+      { title: 'Design principles', href: '/docs/design-principles' },
+    ],
+  },
+]
